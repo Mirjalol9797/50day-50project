f6aa23ce0c0f491309f744d8f24a14e1184a1b0b
Миржалол Мирхомитов Миролим угли <mmirxomitov@office.tm.uz>
2021-02-03 08:51:48 +0500

    2 day

--- Day 1/main.js
+++ Day 1/main.js
@@ -1,26 +1,3 @@
-// const panels = document.querySelectorAll('.panel');
-
-// panels.forEach(function(panel) {
-// 	panel.addEventListener('click', function(){
-// 		removeActiveClasses()
-// 		panel.classList.add('active')
-// 	})
-// })
-
-// function removeActiveClasses() {
-// 	panels.forEach(function(panel){
-// 		panel.classList.remove('active')
-// 	})
-// }
-
-
-// // for(let i=0; i<panels.length; i++) {
-// // 	panels[i].addEventListener('click', function() {
-// // 		console.log('iii')
-// // 	}) 
-// // }
-
-
 let panels = document.querySelectorAll('.panel');
 
 panels.forEach(function(panel) {
